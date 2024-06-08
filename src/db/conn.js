be745ae4643ff501cbbe568a25be7a87b41a8da1
Yashpa12 <yashbhamare1004@gmail.com>
2024-06-09 00:33:18 +0530

    set env file

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const monogoURL = "mongodb://127.0.0.1:27017/yotubeRegistration"
+const monogoURL = process.env.MONGO_URL
 
 
 mongoose.connect(monogoURL).then(()=>{
